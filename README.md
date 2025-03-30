e979524a4ed462e82e7c7667e39f45fb1ac9bd90
Felipe Ozias <oziasfelipe38@gmail.com>
2025-03-30 16:09:38 -0300

    initial-commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# *Project base in React + TypeScript + Vite + tailWindCSS + Biome*
+# *Project Itinerário*
 
 ![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white) ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white) ![Biome](https://img.shields.io/badge/biome-%2300A9C9.svg?style=for-the-badge&logo=biome&logoColor=white)
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,21 @@
 import { Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-import CustomButton from "@components/CustomButton";
+// import CustomButton from "@components/CustomButton";
 import { UserProvider } from "@context/userContext";
 
 function App() {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
 	return (
 		<>
 			<UserProvider>
-				<header className="bg-gray-300  h-10 fixed top-0 w-full flex justify-center items-center">
-					<CustomButton
-						className="mx-1"
-						label="Home"
-						onClick={() => {
-							navigate("/");
-						}}
-					/>
-					<CustomButton
-						className="mx-1"
-						label="Page A"
-						onClick={() => {
-							navigate("/a");
-						}}
-					/>
-					<CustomButton
-						className="mx-1"
-						label="Page B"
-						onClick={() => {
-							navigate("/b");
-						}}
-					/>
-				</header>
 
-				<main className="pt-10 pb-8">
-					<Outlet />
-				</main>
+				<Outlet />
 
-				<footer className="bg-amber-300 flex justify-center items-center h-8 fixed bottom-0 w-full">
-					<p className="italic">Decada Digital | Soluções em tecnologia</p>
-				</footer>
+				{/* <footer className="bg-amber-300 flex justify-center items-center h-8 fixed bottom-0 w-full">
+					<p className="italic">Projeto Itinerário | Soluções em tecnologia</p>
+				</footer> */}
 			</UserProvider>
 		</>
 	);
